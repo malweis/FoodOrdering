cf866913823c8ca3ebe10f4e803f893110c8301a
malweis <58945171+malweis@users.noreply.github.com>
2024-05-09 12:11:55 -0400

    feat: main screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,46 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Image , Text} from 'react-native';
 
 import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
+import products from '@/assets/data/products';
+
+import Colors from '@/src/constants/Colors';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Test</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+     <Image
+      source={{ uri: products[0].image }}
+      style={styles.image}
+     />
+    <Text style={styles.title}>{products[0].name}</Text>
+    <Text style={styles.price}>{products[0].price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Colors.light.background,
+    padding: 10,
+    borderRadius: 20,
   },
+  image :{
+    width: "100%",
+    aspectRatio: 1,
+  },
+  
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 18,
+    fontWeight:"600",
+    marginVertical: 10,
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  price : {
+    color: Colors.light.tint,
+    fontWeight: "bold",
   },
 });
