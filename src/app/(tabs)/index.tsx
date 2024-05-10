47b33418d7abb3cfc5c8bfedf787f4a3bb4d58f8
malweis <58945171+malweis@users.noreply.github.com>
2024-05-10 11:30:52 -0400

    feat: expo router screens

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,25 +1,5 @@
-import { StyleSheet, ScrollView, FlatList} from 'react-native';
+import { Redirect } from 'expo-router';
 
-
-import products from '@assets/data/products';
-
-import Colors from '@constants/Colors';
-import ProductListItem from '@components/ProductListItem';
-
-
-export default function TabOneScreen() {
-  return (
-    <FlatList  data={products} renderItem={ ({item }) => <ProductListItem product={item}/> } numColumns={2} contentContainerStyle={styles.container} columnWrapperStyle={{gap: 10}} />
-  
-
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: Colors.light.tabIconDefault,
-    padding: 10,
-    gap: 10,
-  },
-
-});
+export default function TabIndex () {
+  return <Redirect href={'/menu/'} />;
+};
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,59 +1,60 @@
-import { StyleSheet, View, Image , Text} from 'react-native';
+import { StyleSheet, View, Image, Text, Pressable } from "react-native";
 
+import products from "@assets/data/products";
 
-import products from '@assets/data/products';
+import Colors from "@constants/Colors";
+import { Product } from "@constants/types";
+import { Link } from "expo-router";
 
-import Colors from '@constants/Colors';
-import { Product } from '@constants/types';
-
-export const  defaultPizzaImage = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png"
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
 interface ListProps {
   product: Product;
 }
 
- const  ProductListItem = ({ product } : ListProps ) => {
-    return (
-        <View style={styles.container}>
-     <Image
-      source={{ uri: product.image || defaultPizzaImage}}
-      style={styles.image}
-    resizeMode='contain'
-     />
-    <Text style={styles.title}>{product.name}</Text>
-    <Text style={styles.price}>{product.price}</Text>
-    </View>
-    );
-    
-}
+const ProductListItem = ({ product }: ListProps) => {
+  return (
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>{product.price}</Text>
+      </Pressable>
+    </Link>
+  );
+};
 export default ProductListItem;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: Colors.light.background,
-      padding: 10,
-      borderRadius: 20,
-      maxWidth: "50%",
-      
-    },
-    image :{
-      width: "100%",
-      aspectRatio: 1,
-    },
-    
-    title: {
-      fontSize: 18,
-      fontWeight:"600",
-      marginVertical: 10,
-    },
-    separator: {
-      marginVertical: 30,
-      height: 1,
-      width: '80%',
-    },
-    price : {
-      color: Colors.light.tint,
-      fontWeight: "bold",
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: Colors.light.background,
+    padding: 10,
+    borderRadius: 20,
+    maxWidth: "50%",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+});
