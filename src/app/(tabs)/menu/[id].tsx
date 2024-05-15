7a8d1dc58f386c8d68afcd521f77582ae2b7b4ed
malweis <58945171+malweis@users.noreply.github.com>
2024-05-15 11:21:06 -0400

    feat: products details screen (#2)
    adds controls and buttons

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,41 +1,91 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import React from 'react'
-import { Stack, useLocalSearchParams } from 'expo-router'
-import products from '@assets/data/products'
-import { defaultPizzaImage } from '@/components/ProductListItem'
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import React from "react";
+import { Stack, useLocalSearchParams } from "expo-router";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import Button from "@/components/Button";
 
 const ProductDetailScreen = () => {
-  const { id } = useLocalSearchParams()
-  const  product = products.find((p) => p.id.toString() === id)
+  const sizes = ["S", "M", "L", "XL"];
+  const [selectedSize, setSelectedSize] = React.useState(sizes[0]);
+  const { id } = useLocalSearchParams();
+  const product = products.find((p) => p.id.toString() === id);
 
   if (!product) {
-    return <Text>Product not found</Text>
+    return <Text>Product not found</Text>;
+  }
+  const addToCart = () => {
+    console.warn("Add to cart");
   }
 
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: product.name }} />
-      <Image source={{uri : product.image || defaultPizzaImage}} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text>Select size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <Pressable
+          onPress={() => setSelectedSize(size)}
+            style={[
+              styles.size,
+              {
+                backgroundColor: selectedSize === size ? "gainsboro" : "white",
+              },
+            ]}
+            key={size}
+          >
+            <Text
+              style={[
+                styles.sizeText,
+                { color: selectedSize === size ? "black" : "gray" },
+              ]}
+            >
+              {size}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
       <Text style={styles.price}>${product.price}</Text>
+      <Button text="Add to cart" onPress={addToCart} />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container: { 
-    backgroundColor: 'white',
+  container: {
+    backgroundColor: "white",
     flex: 1,
     padding: 10,
   },
   price: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    marginTop: 'auto',
   },
   image: {
-    width: '100%',
+    width: "100%",
     aspectRatio: 1,
   },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+});
 
-})
-
-export default ProductDetailScreen
+export default ProductDetailScreen;
