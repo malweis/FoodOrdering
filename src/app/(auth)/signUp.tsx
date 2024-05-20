1afe864fc65f82e629c367914a3459175b6b0e45
Federico Luis Molas Sanchez <fmolas@roshka.com>
2024-05-20 14:32:23 -0400

    feat: Auth Screen

---
+++ src/app/(auth)/signUp.tsx
@@ -0,0 +1,94 @@
+import { View, Text, StyleSheet, TextInput, Image, Alert } from 'react-native'
+import React from 'react'
+import Button from '@/components/Button'
+import Colors from '@/constants/Colors'
+
+import { Stack, router } from 'expo-router'
+
+const SingUp = () => {
+
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    const [error, setError] = React.useState('')
+    
+
+    const resetFields = () => {
+        setEmail('')
+        setPassword('')
+    }
+
+    const validateInputs = () => {
+        setError('')
+        if (!email || !password) {
+            setError('Please fill all the fields')
+            return false
+        }
+
+      
+
+        return true
+    }
+
+
+    
+
+    const onSubmit = () => {
+        if (!validateInputs()) return
+        
+        console.warn('Sign Up', {email, password})
+        resetFields()
+    }
+
+
+
+   
+
+
+
+
+  return (
+    <View style={styles.container}>
+        <Stack.Screen options={{ title: 'Sign Up'}} />
+       
+        <Text style={styles.label}>Email</Text>
+      <TextInput value={email} onChangeText={setEmail}  placeholder='john@gmail.com' style={styles.input} textContentType='emailAddress' keyboardType='email-address' />
+      <Text style={styles.label}>Password</Text>
+      <TextInput value={password} onChangeText={setPassword}  secureTextEntry style={styles.input} textContentType='password' />
+        <Text style={{color: 'red'}}>{error}</Text>
+     <Button  onPress={onSubmit} text={'Create account' } />
+   <Text onPress={() => router.replace('/signIn')} style={styles.textButton}>Sign in</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container :{
+        flex: 1,
+        justifyContent: 'center',
+        padding: 10,
+
+    },
+    input: {
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 5,
+        marginBottom: 20,
+     
+    },
+    label: {
+       color: 'gray',
+       fontSize: 16,
+    },
+ 
+    textButton: {
+        color: Colors.light.tint,
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        marginVertical: 10,
+    }
+
+})
+
+export default SingUp
