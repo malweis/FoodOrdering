d5bd3c50f0a1509663409d283beab8672d5c7b01
Federico Luis Molas Sanchez <fmolas@roshka.com>
2024-05-27 11:49:31 -0400

    fix: rompe top navigation

--- src/app/(admin)/orders/list/_layout.tsx
+++ src/app/(admin)/orders/list/_layout.tsx
@@ -1,13 +1,17 @@
-import { Tabs, withLayoutContext } from "expo-router";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { withLayoutContext } from 'expo-router';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const TopTabs = withLayoutContext(createMaterialTopTabNavigator().Navigator) ;
+export const TopTabs = withLayoutContext(
+  createMaterialTopTabNavigator().Navigator
+);
 
-export default function OrderListNavigator(){
+export default function OrdersTabs() {
   return (
-    <TopTabs>
-      <Tabs.Screen name="index" options={{ title: 'active="--"' }} />
-      
-    </TopTabs>
-  )
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }} edges={['top']}>
+      <TopTabs>
+        <TopTabs.Screen name="index" options={{ title: 'Active' }} />
+      </TopTabs>
+    </SafeAreaView>
+  );
 }
