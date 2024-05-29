9f7f35faafc1faf6b790cff0ec8603ec7dfbbd4d
malweis <58945171+malweis@users.noreply.github.com>
2024-05-29 07:04:26 -0400

    feat: products query on user side

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -5,9 +5,22 @@ import products from '@assets/data/products';
 
 import Colors from '@constants/Colors';
 import ProductListItem from '@components/ProductListItem';
+import { useEffect } from 'react';
+import { supabase } from '@/lib/supabase';
 
 
 export default function TabOneScreen() {
+  useEffect(() => {
+    const  fetchProducts = async () => {
+     const {data, error} = await supabase.from('products').select('*');
+     console.log(data, error)
+    }
+
+    fetchProducts();
+
+  }, [])
+
+
   return (
     <FlatList  data={products} renderItem={ ({item }) => <ProductListItem product={item}/> } numColumns={2} contentContainerStyle={styles.container} columnWrapperStyle={{gap: 10}} />
   
