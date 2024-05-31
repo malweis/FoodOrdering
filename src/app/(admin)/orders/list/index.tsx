9b7f4a05dd74fdfca45d67261b60eecc3f1e04ae
Federico Luis Molas Sanchez <fmolas@roshka.com>
2024-05-31 10:40:19 -0400

    feat: realtime update order list

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,25 +1,52 @@
-import { StyleSheet, FlatList, ActivityIndicator, Text} from 'react-native';
+import { StyleSheet, FlatList, ActivityIndicator, Text } from "react-native";
 
+import Colors from "@constants/Colors";
 
-
-import Colors from '@constants/Colors';
-
-import OrderListItem from '@/components/OrderListItem';
-import { useOrderList } from '@/api/orders';
-
+import OrderListItem from "@/components/OrderListItem";
+import { useOrderList } from "@/api/orders";
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabase";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function TabOneScreen() {
-  const {data : orders, error, isLoading} =useOrderList({archived : false});
+  const { data: orders, error, isLoading } = useOrderList({ archived: false });
+
+ const queryClient = useQueryClient();
+
+  useEffect(() => {
+    const orders = supabase
+      .channel('custom-insert-channel')
+      .on(
+        'postgres_changes',
+        { event: 'INSERT', schema: 'public', table: 'orders' },
+        (payload) => {
+          console.log('Change received!', payload);
+          queryClient.invalidateQueries({queryKey: ['orders']})
+         
+        }
+      )
+      .subscribe();
+    return () => {
+      orders.unsubscribe();
+    };
+  }, []);
+
   if (isLoading) {
     return <ActivityIndicator />;
   }
   if (error) {
     return <Text>Failed to fetch product</Text>;
   }
-  return (
-    <FlatList  data={orders} renderItem={ ({item }) => <OrderListItem order={item}/> } numColumns={1} contentContainerStyle={styles.container}  />
-  
 
+
+
+  return (
+    <FlatList
+      data={orders}
+      renderItem={({ item }) => <OrderListItem order={item} />}
+      numColumns={1}
+      contentContainerStyle={styles.container}
+    />
   );
 }
 
@@ -29,8 +56,5 @@ const styles = StyleSheet.create({
     padding: 10,
     gap: 10,
     flex: 1,
-
-    
   },
-
 });
